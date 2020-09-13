8cd5173bf1c669e858f577ab4958dd59de62d23e
Rithwik <rithuv20@gmail.com>
2020-09-13 12:56:07 +0530

    committed

--- app/src/main/java/com/example/cargame/GameCanvas.java
+++ app/src/main/java/com/example/cargame/GameCanvas.java
@@ -20,16 +20,7 @@ public class GameCanvas extends View {
 
     private Canvas c;
     private float startX = 0, startY = 0, stopX = 0, stopY = 0, touchX = 0, touchY = 0;
-    private PathMeasure pathMeasure;
-    int bm_offsetX, bm_offsetY;
-    float iCurStep = 0;
-    float distance;
-    float pathLength;
 
-    float[] pos;
-    float[] tan;
-
-    Matrix matrix;
     private Paint blackPaint, whitePaint, greenPaint, redPaint;
     private Path touchPath;
     private float width, height;
@@ -72,15 +63,6 @@ public class GameCanvas extends View {
         redPaint.setColor(Color.RED);
 
         imageBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.car_image);
-        bm_offsetX = imageBitmap.getWidth()/2;
-        bm_offsetY = imageBitmap.getHeight()/2;
-
-        pathMeasure = new PathMeasure(touchPath, false);
-        pathLength = pathMeasure.getLength();
-        iCurStep = 1;
-        distance = 0;
-        pos = new float[2];
-        tan = new float[2];
 
 
     }
@@ -112,46 +94,30 @@ public class GameCanvas extends View {
         canvas.drawCircle(width/2, height/6,80,redPaint);
         canvas.drawCircle(width/2, 5*height/6,100,greenPaint);
 
-        if(distance < pathLength){
-            pathMeasure.getPosTan(distance, pos, tan);
 
-            matrix.reset();
-            float degrees = (float)(Math.atan2(tan[1], tan[0])*180.0/Math.PI);
-            matrix.postRotate(degrees, bm_offsetX, bm_offsetY);
-            matrix.postTranslate(pos[0]-bm_offsetX, pos[1]-bm_offsetY);
-
-            canvas.drawBitmap(imageBitmap, matrix, null);
-
-            distance += iCurStep;
-        }else{
-            distance = 0;
-        }
-//        invalidate();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         touchX = event.getX();
         touchY = event.getY();
-            if (event.getAction() == MotionEvent.ACTION_DOWN ) {
-                 startX = touchX; startY = touchY;
-                if(checkStartTouchPoint(startX,startY))
-                    touchPath.moveTo(touchX, touchY);
-            } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
-                if(checkStartTouchPoint(startX,startY))
-                    touchPath.lineTo(touchX, touchY);
+        if (event.getAction() == MotionEvent.ACTION_DOWN ) {
+            startX = touchX; startY = touchY;
+            if(checkStartTouchPoint(startX,startY))
+                touchPath.moveTo(touchX, touchY);
+        } else if (event.getAction() == MotionEvent.ACTION_MOVE) {
+            if(checkStartTouchPoint(startX,startY))
+                touchPath.lineTo(touchX, touchY);
 
 
 
-            }else if (event.getAction() == MotionEvent.ACTION_UP) {
-//                float touchX = event.getX();
-//                float touchY = event.getY();
-                 stopX = touchX; stopY = touchY;
-                if(checkStopTouchPoint(stopX,stopY))
-                    touchPath.lineTo(touchX, touchY);
-                c.drawPath(touchPath, blackPaint);
-                touchPath = new Path();
-            }  else return false;
+        }else if (event.getAction() == MotionEvent.ACTION_UP) {
+            stopX = touchX; stopY = touchY;
+            if(checkStopTouchPoint(stopX,stopY))
+                touchPath.lineTo(touchX, touchY);
+            c.drawPath(touchPath, blackPaint);
+            touchPath = new Path();
+        }  else return false;
         if(checkStartTouchPoint(startX,startY) || checkStopTouchPoint(stopX, stopY))
             invalidate();
         return true;
